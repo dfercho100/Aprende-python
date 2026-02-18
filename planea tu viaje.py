c1b2bb7ba4de6067586d65460ea05bc909e366bb
dcfercho100 <dfcoronado@hotmail.com>
2026-02-18 16:16:19 -0500

    actualizados

--- planea tu viaje.py
+++ planea tu viaje.py
@@ -1,17 +1,23 @@
-distance_mi = 8
-is_raining = False
-has_bike = False
-has_car = True
-has_ride_share_app = False
-
+distance_mi= 5
+is_raining= True  
+has_bike= True
+has_car= False 
+has_ride_share_app = True 
 if not distance_mi:
-    print("False")
-
-if distance_mi <= 1 and not is_raining:
-    print("True")
-elif (distance_mi > 1 or distance_mi <= 6) and not is_raining and has_bike:
-    print("True")
-elif distance_mi > 6 and (not has_car or not has_ride_share_app):
-    print("True")
+    print(False)
+elif distance_mi <= 1:
+        if not is_raining:
+            print(True) 
+        else:
+            print(False)
+elif distance_mi <= 6:
+    if has_bike and not is_raining:
+        print(True)
+    else:
+        print(False)
 else:
-    print("False")
+    if has_car or has_ride_share_app:
+        print(True)
+    else:
+        print(False) 
+# salio False, lo que es correcto, ya que la distancia es de 5 millas, y aunque tiene una app de ride share, esta lloviendo, por lo que no es una buena opcion para viajar. un carro seria una mejor opcion, pero no tiene uno.
